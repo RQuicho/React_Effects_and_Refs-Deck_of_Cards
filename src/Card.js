0f3d7613d3af3510dab0e4d525d56bfea1c40eca
RQuicho <raymondquicho@gmail.com>
2023-10-15 17:47:30 -0700

    adding button that draws a card from the same deck

--- src/Card.js
+++ src/Card.js
@@ -3,33 +3,42 @@ import axios from 'axios';
 
 const Card = () => {
   const [cardData, setCardData] = useState(null);
-  // const [draw, setDraw] = useState('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
-  const drawCard = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
+  const [deckId, setDeckId] = useState(null);
+  const drawFirstCard = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
+
   useEffect(function loadDataWhenMounted() {
-    async function loadData () {
-      const res = await axios.get(drawCard);
-      setCardData(res.data);
+    try {
+      async function loadData () {
+        const res = await axios.get(drawFirstCard);
+        setCardData(res.data);
+        setDeckId(res.data.deck_id);
+      }
+      loadData();
+    } catch (err) {
+      console.log(err);
     }
-    loadData();
   }, []);
 
-  // async function loadData () {
-  //   const res = await axios.get(drawCard);
-  //   setCardData(res.data);
-  //   console.log(cardData);
-  // }
-  // loadData();
+  console.log('cardData...', cardData);
+  console.log('deckId...', deckId);
 
-  console.log(cardData);
+  const drawCardFromSameDeck = async () => {
+    if (deckId) {
+      const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+      setCardData(res.data);
+    }
+  }
+
   return (
     <div>
-      {cardData ? <h1>Deck Id: {cardData.deck_id}</h1> : <h1>Loading...</h1>}
-      {cardData ? <h1>Value: {cardData.cards[0].value}</h1> : <h1>Loading...</h1>}
-      {cardData ? <h1>Suit: {cardData.cards[0].suit}</h1> : <h1>Loading...</h1>}
-      {cardData ? <img src= {cardData.cards[0].image} /> : <h1>Loading...</h1>}
+      {/* {cardData ? <h1>Deck Id: {cardData.deck_id}</h1> : <h1>Loading...</h1>} */}
+      {/* {cardData ? <h1>Value: {cardData.cards[0].value}</h1> : <h1>Loading...</h1>}
+      {cardData ? <h1>Suit: {cardData.cards[0].suit}</h1> : <h1>Loading...</h1>} */}
+      {cardData ? <h1>Cards Remaining: {cardData.remaining}</h1> : <h1>Loading...</h1>}
 
+      {cardData ? <img src= {cardData.cards[0].image} alt='Card'/> : <h1>Loading...</h1>}
+      <button onClick={drawCardFromSameDeck}>Draw</button>
 
-      {/* <p>Deck Id: {cardData}</p> */}
     </div>
   )
 }
