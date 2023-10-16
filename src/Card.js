36dc0f1a88a0d5c88d695bde2d860c982ac3ff73
RQuicho <raymondquicho@gmail.com>
2023-10-15 17:05:52 -0700

    properly getting data from api

--- src/Card.js
+++ src/Card.js
@@ -5,17 +5,31 @@ const Card = () => {
   const [cardData, setCardData] = useState(null);
   // const [draw, setDraw] = useState('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
   const drawCard = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
-  useEffect(() => {
+  useEffect(function loadDataWhenMounted() {
     async function loadData () {
       const res = await axios.get(drawCard);
-      setCardData(res);
+      setCardData(res.data);
     }
     loadData();
   }, []);
 
+  // async function loadData () {
+  //   const res = await axios.get(drawCard);
+  //   setCardData(res.data);
+  //   console.log(cardData);
+  // }
+  // loadData();
+
+  console.log(cardData);
   return (
     <div>
-      <p>{cardData}</p>
+      {cardData ? <h1>Deck Id: {cardData.deck_id}</h1> : <h1>Loading...</h1>}
+      {cardData ? <h1>Value: {cardData.cards[0].value}</h1> : <h1>Loading...</h1>}
+      {cardData ? <h1>Suit: {cardData.cards[0].suit}</h1> : <h1>Loading...</h1>}
+      {cardData ? <img src= {cardData.cards[0].image} /> : <h1>Loading...</h1>}
+
+
+      {/* <p>Deck Id: {cardData}</p> */}
     </div>
   )
 }
